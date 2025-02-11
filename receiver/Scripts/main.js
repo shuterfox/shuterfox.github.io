e361c69cf1fe2301b362e00dd02a8d6693f3cd1f
shuterfox <theo.bagieu@gmail.com>
2025-02-11 19:05:36 +0100

    revert to old app

--- receiver/Scripts/main.js
+++ receiver/Scripts/main.js
@@ -1,6 +1,26 @@
+
+
+
 var NightState = false;
 
-const context = cast.framework.CastReceiverContext.getInstance();
+const context = cast.framework.CastReceiverContext.getInstance().start();
+const playerManager = context.getPlayerManager();
+
+// Debug Logger
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
+
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+    if (!castDebugLogger.debugOverlayElement_) {
+        castDebugLogger.setEnabled(true);
+    }
+});
+
+castDebugLogger.loggerLevelByEvents = {
+    'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+    'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+};
 
 context.addCustomMessageListener("urn:x-cast:com.werewolvesCompanion.changeState", (event) => {
     console.log("Received Change StateEvent")
